f5e0c349fedf145a4e26427c193e548fd470ce12
Logan Lamoureux <logalamo@gmail.com>
2020-03-25 18:54:27 -0500

    Photo Upload Page form created
    Rough Draft of the photo upload form.

---
+++ fileUpload.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Photo Upload</title>
+</head>
+<body>
+    <form action="POST" enctype="multipart/form-data">
+        <label for="name">Photo Name: </label>
+        <input type="text" name="name" id="name">
+        <label for="description">Description: </label>
+        <input type="text" name="description" id="description">
+        <label for="genre">Select a genre: </label>
+        <select id="genre"></select>
+        <label for="image">Image: </label>
+        <input type="file" name="image" id="image">
+    </form>
+</body>
+</html>
